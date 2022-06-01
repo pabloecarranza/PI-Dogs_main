17a80605b20f25da7459fceb37b926b814c487d7
pabloecarranza <63490339+pabloecarranza@users.noreply.github.com>
2022-06-01 03:17:13 -0300

    deploy test 4

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen((PORT, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
